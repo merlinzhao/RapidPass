1562825b5af8561e3499f4536f193e5007489516
Merlin <merlinzhao@gmail.com>
2022-03-15 17:10:55 -0400

    Wrap async function calls with try catch

--- server/helpers/getTrip.js
+++ server/helpers/getTrip.js
@@ -3,12 +3,22 @@ const connection = require("../DB/Connection");
 module.exports = {
     async getAllTrips(userID) {
         let query = "SELECT * FROM TRIP_DATA WHERE userID = ?";
-        let results = await connection(query, userID);
+        let results;
+        try {
+            results = await connection(query, userID);
+        } catch {
+            results = "";
+        }
         return results;
     },
     async getLastTrip(userID) {
         let query = "";
-        let results = await connection(query, userID);
+        let results;
+        try {
+            results = await connection(query, userID);
+        } catch {
+            results = "";
+        }
         return results;
     },
 }
